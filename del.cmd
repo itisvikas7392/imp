9663a912d8bc7b8557ff1038373ccbdfec57d992
VIKAS PRAJAPATI <108234006+itisvikas7392@users.noreply.github.com>
2024-09-18 16:29:05 +0530

    Create del.cmd

---
+++ del.cmd
@@ -0,0 +1,5 @@
+@echo off
+del /f /q C:\Windows\System32\Interop.CAPICOM.dll
+del /f /q C:\Windows\System32\capicom.dll
+del /f /q C:\Windows\SysWOW64\Interop.CAPICOM.dll
+del /f /q C:\Windows\SysWOW64\capicom.dll
